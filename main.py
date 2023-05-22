509800b5c9967708d670ff43fe85aba36b8f2d11
gk <gk@adamant.local>
2023-05-22 19:05:50 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+num1 = int(input())
+if num1 % 7 == 0:
+    print('Воскресенье')
+elif num1 % 7 == 1:
+    print('Понедельник')
+elif num1 % 7 == 2:
+    print('Вторник')
+elif num1 % 7 == 3:
+    print('Среда')
+elif num1 % 7 == 4:
+    print('Четверг')
+elif num1 % 7 == 5:
+    print('Пятница')
+elif num1 % 7 == 6:
+    print('Суббота')
